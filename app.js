314051c936279f03865f9f83acf147c9907a6885
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-25 15:11:52 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// types
+// types ---> 1) custom types and interface
 var student = [];
 var teachers = [];
+var teachers1 = [];
 var student1 = { id: 1, name: "kamal", age: 25, faculty: 'CSE' };
 var student2 = { id: 2, name: "Amal", age: 24, faculty: 'TONIC' };
 student.push(student1);
--- app.ts
+++ app.ts
@@ -1,9 +1,18 @@
-// types
+// types ---> 1) custom types and interface
 
 type Graduate = {id:number,name:string,age:number,faculty:string,uni?:string}
 
+interface Graduate2{
+    id:number,
+    name:string,
+    age:number,
+    faculty:string
+}
+
 let student:Graduate []= [];
 let teachers:Graduate []= [];
+
+let teachers1:Graduate2 []= [];
 
 let student1={id:1,name:"kamal",age:25,faculty:'CSE'};
 let student2={id:2,name:"Amal",age:24,faculty:'TONIC'};
