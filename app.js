3f43ec7fe2d38de94849ec9f1fc19cdeaed15b0e
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-25 16:05:24 +0530

    update tuple

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
-// classes
-var person = /** @class */ (function () {
-    function person(name, age, nic) {
-        this.name = name;
-        this.age = age;
-        this.nic = nic;
-    }
-    return person;
-}());
-var person1 = new person("kamal", 20, "232323233");
-console.log(person1);
-console.log(person1.name);
-// console.log(person1.nic); cant access
-var DBConnection = /** @class */ (function () {
-    function DBConnection(value) {
-        DBConnection.connection = value;
-    }
-    DBConnection.getConnection = function () {
-        if (!DBConnection.connection) {
-            new DBConnection("test connection");
-        }
-        return DBConnection.connection;
-    };
-    return DBConnection;
-}());
-var connection = DBConnection.getConnection();
-console.log(connection);
+// tuple
+var data = ["Aaa", 123, "Bbb", true, 456];
