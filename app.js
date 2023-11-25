3374ccf7179c0561cb17b983c32f8ac0a7e21c71
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-25 11:37:29 +0530

    ts compile

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello Typescript");
+function sum(num1, num2) {
+    return num1 + num2;
+}
+var n1 = '10';
+var n2 = 20;
+// +n1 + convert to string to number
+var value1 = sum(+n1, +n2);
+console.log("value is :" + value1);
+var value2 = sum(+n1, +n2);
+console.log("value is :" + value2);
--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
 console.log("Hello Typescript");
+
+function sum(num1:number, num2:number) {
+    return num1+num2;
+}
+
+let n1='10';
+let n2=20;
+
+// +n1 + convert to string to number
+
+let value1 = sum(+n1,+n2);
+console.log("value is :"+value1);
+
+let value2 = sum(+n1,+n2);
+console.log("value is :"+value2);
