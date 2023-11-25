dc07af6ce4bbf0e1bbfe402fe6346ea194c80f10
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-25 11:56:28 +0530

    updated

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 console.log("Hello Typescript");
-function sum(num1, num2) {
-    return num1 + num2;
+// primitives - number, string, boolean
+function sendSum(num1, num2, name, isWithName) {
+    return isWithName ? "Hi , ".concat(name, " Sum is: ").concat(num1 + num2) : "Sum is: ".concat(num1 + num2);
 }
 var n1 = '10';
 var n2 = 20;
-// +n1 + convert to string to number
-var value1 = sum(+n1, +n2);
+// +n1 converts string to number
+var value1 = sendSum(+n1, +n2, "kamal", true);
 console.log("value is :" + value1);
-var value2 = sum(+n1, +n2);
+var value2 = sendSum(+n1, +n2, "kamal", false); // changed isWithName to false
 console.log("value is :" + value2);
--- app.ts
+++ app.ts
@@ -1,16 +1,18 @@
 console.log("Hello Typescript");
 
-function sum(num1:number, num2:number) {
-    return num1+num2;
+// primitives - number, string, boolean
+
+function sendSum(num1: number, num2: number, name: string, isWithName: boolean) {
+    return isWithName ? `Hi , ${name} Sum is: ${num1 + num2}` : `Sum is: ${num1 + num2}`;
 }
 
-let n1='10';
-let n2=20;
+let n1 = '10';
+let n2 = 20;
 
-// +n1 + convert to string to number
+// +n1 converts string to number
 
-let value1 = sum(+n1,+n2);
-console.log("value is :"+value1);
+let value1 = sendSum(+n1, +n2, "kamal", true);
+console.log("value is :" + value1);
 
-let value2 = sum(+n1,+n2);
-console.log("value is :"+value2);
+let value2 = sendSum(+n1, +n2, "kamal", false); // changed isWithName to false
+console.log("value is :" + value2);
