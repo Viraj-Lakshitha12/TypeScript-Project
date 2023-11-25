e186824ebfd65c3c8b49504809b5197887e236fa
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-25 15:21:54 +0530

    create classes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-// types ---> 1) custom types and interface
-var student = [];
-var teachers = [];
-var teachers1 = [];
-var student1 = { id: 1, name: "kamal", age: 25, faculty: 'CSE' };
-var student2 = { id: 2, name: "Amal", age: 24, faculty: 'TONIC' };
-student.push(student1);
-student.push(student2);
+// classes
+var person = /** @class */ (function () {
+    function person(name, age, nic) {
+        this.name = name;
+        this.age = age;
+        this.nic = nic;
+    }
+    return person;
+}());
+var person1 = new person("kamal", 20, "232323233");
+console.log(person1);
