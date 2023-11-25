a43f604099dbc38239cf0673785bd5204e15e980
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-25 14:53:38 +0530

    create arrays

--- app.js
+++ app.js
@@ -1,39 +1,13 @@
-// @ts-ignore
-var student;
-student = { id: 1, name: "amal", age: 30, salary: 50000 };
-// student={id:1,name:"amal",age:30,salary:50000,collage:"ABC"};
-// student=10;
-console.log(student);
-//----------------------------------------- distance calculator -------------------------------------------------------------------
-function calcDistance(location1, location2) {
-    var lon1 = location1.lon;
-    var lat1 = location1.lat;
-    var lon2 = location2.lon;
-    var lat2 = location2.lat;
-    // ---- calc distance  ----
-    var dLat = lat2 - lat1;
-    var dLon = lon2 - lon1;
-    var a = Math.pow(Math.sin(dLat / 2), 2) +
-        Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dLon / 2), 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    // Radius of the Earth in kilometers (mean value)
-    var R = 6371;
-    // Calculate the distance
-    return R * c;
-}
-//
-// let location1 = {lon:80.7718,lat:7.8731}
-// let location2 = {lon:133.7751,lat:25.2744}
-var lat1Input = document.getElementById('lat1');
-var lon1Input = document.getElementById('lon1');
-var lat2Input = document.getElementById('lat2');
-var lon2Input = document.getElementById('lon2');
-var btnElement = document.getElementById('btn');
-var resultElement = document.getElementById('result');
-btnElement.addEventListener('click', function () {
-    var location1 = { lon: +lat1Input.value, lat: +lon1Input.value };
-    var location2 = { lon: +lat2Input.value, lat: +lon2Input.value };
-    var distance = calcDistance(location1, location2);
-    console.log(distance);
-    resultElement.innerText = "".concat(distance.toFixed(2), "  KM");
-});
+// Arrays
+var num_array = [1, 2, 3];
+var string_array = ["amal", "kamal", "nimal"];
+var num_string_array = [1, 2, "amal", "kamal", "nimal"];
+var student1 = { id: 1, name: "kamal", age: 25, faculty: 'CSE' };
+var student2 = { id: 2, name: "Amal", age: 24, faculty: 'TONIC' };
+var student3 = 10;
+var student4 = { id: 3, name: "Amara", age: 24, faculty: 'TONIC', uni: 'Uoc' };
+var student = [];
+student.push(student1);
+student.push(student2);
+// student.push(student3); cant push
+student.push(student4);
--- app.ts
+++ app.ts
@@ -2,4 +2,16 @@
 
 let num_array :number[] = [1,2,3];
 let string_array :string[] = ["amal","kamal","nimal"];
+let num_string_array :(number | string)[] = [1,2,"amal","kamal","nimal"];
+
+let student1={id:1,name:"kamal",age:25,faculty:'CSE'};
+let student2={id:2,name:"Amal",age:24,faculty:'TONIC'};
+let student3=10;
+let student4={id:3,name:"Amara",age:24,faculty:'TONIC',uni:'Uoc'};
+
+let student:{id:number,name:string,age:number,faculty:string,uni?:string} []= [];
+student.push(student1);
+student.push(student2);
+// student.push(student3); cant push
+student.push(student4);
 
