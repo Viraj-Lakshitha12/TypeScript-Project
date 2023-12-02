04da3d93d785213542bce31ef0a3191e5f3a397d
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-02 10:22:53 +0530

    create union.ts file

---
+++ app.ts
@@ -0,0 +1,22 @@
+//union
+
+let array:(string|number|boolean) []=[1,2,3,"Hello",true];
+
+class Student{
+    id:number
+    name:string
+    collage:string
+    faculty:string
+}
+class Teacher{
+    id:number
+    name:string
+    subject:string
+    collage:string
+}
+
+class Soldier{
+    id:number
+    name:string
+    post:string
+}
