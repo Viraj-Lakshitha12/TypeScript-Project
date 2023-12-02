0998a6b616fe0a9cb01e9b136870f584626123a6
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-02 10:27:34 +0530

    create enum.ts file

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-//union
-var array = [1, 2, 3, "Hello", true];
-var Student = /** @class */ (function () {
-    function Student() {
-    }
-    return Student;
-}());
-var Teacher = /** @class */ (function () {
-    function Teacher() {
-    }
-    return Teacher;
-}());
-var Soldier = /** @class */ (function () {
-    function Soldier() {
-    }
-    return Soldier;
-}());
-var array1 = [];
+//enum
--- app.ts
+++ app.ts
@@ -1,23 +1,2 @@
-//union
+//enum
 
-let array:(string|number|boolean) []=[1,2,3,"Hello",true];
-
-class Student{
-    id:number
-    name:string
-    collage:string
-    faculty:string
-}
-class Teacher{
-    id:number
-    name:string
-    subject:string
-    collage:string
-}
-
-class Soldier{
-    id:number
-    name:string
-    post:string
-}
-let array1 : (Student|Soldier|Teacher)[]=[];
