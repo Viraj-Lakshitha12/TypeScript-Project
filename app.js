62844bb1100431376f4e5aa6067ee8e15d80f9b2
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-02 10:04:50 +0530

    update bank.html file

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
 var ownerName_inputElement = document.getElementById('ownerName');
 var fixedAmount_inputElement = document.getElementById('fixedAmount');
+var newInterestRate_inputElement = document.getElementById('newInterestRate');
 var addFix_btn = document.getElementById('addFixBtn');
+var updateInterestBtn = document.getElementById('updateInterestBtn');
+var table_body = document.getElementById('fixedDepositTableBody');
+var rate = +12.5;
+var accounts = [];
+var new_accounts = [];
 addFix_btn.addEventListener('click', function () {
     var ownerName = ownerName_inputElement.value;
     var fixedAmount = fixedAmount_inputElement.value;
     console.log("owner : ".concat(ownerName, " , fixed amount :").concat(fixedAmount));
+    var annual_interest = (+fixedAmount * 12.5) / 100;
+    var account = new Account_Details(ownerName, +fixedAmount, rate, annual_interest);
+    accounts.push(account);
+    console.log(accounts);
+    updateTable();
 });
+updateInterestBtn.addEventListener('click', function () {
+    var new_rate = newInterestRate_inputElement.value;
+    rate = +new_rate;
+    // for (let i = 0; i < accounts.length; i++) {
+    //    accounts[i].interest_rate=rate;
+    // }
+    accounts.map(function (r) {
+        r.interest_rate = rate;
+        r.annual_interest = (r.fixed_amount * r.interest_rate) / 100;
+        new_accounts.push(r);
+    });
+    accounts = new_accounts;
+    updateTable();
+});
+function updateTable() {
+    table_body.innerHTML = "";
+    accounts.map(function (r) {
+        var record_element = document.createElement('tr');
+        var record_data = "<td>".concat(r.owner_name, "</td>\n                         <td>").concat(r.fixed_amount, "</td>\n                         <td>").concat(r.interest_rate, "</td>\n                         <td>").concat(r.annual_interest, "</td>");
+        record_element.innerHTML = record_data;
+        table_body.appendChild(record_element);
+    });
+}
+var Account_Details = /** @class */ (function () {
+    function Account_Details(owner_name, fixed_amount, interest_rate, annual_interest) {
+        this.owner_name = owner_name;
+        this.fixed_amount = fixed_amount;
+        this.interest_rate = interest_rate;
+        this.annual_interest = annual_interest;
+    }
+    return Account_Details;
+}());
--- app.ts
+++ app.ts
@@ -1,18 +1,58 @@
 
 let ownerName_inputElement = document.getElementById('ownerName') as HTMLInputElement;
 let fixedAmount_inputElement = document.getElementById('fixedAmount') as HTMLInputElement;
+let newInterestRate_inputElement = document.getElementById('newInterestRate') as HTMLInputElement;
 
 let addFix_btn = document.getElementById('addFixBtn');
+let updateInterestBtn = document.getElementById('updateInterestBtn');
+let table_body = document.getElementById('fixedDepositTableBody');
+
+let rate = +12.5;
+let accounts:Account_Details [] = [];
+let new_accounts:Account_Details [] = [];
 
 addFix_btn.addEventListener('click',() =>{
    let ownerName =  ownerName_inputElement.value;
    let fixedAmount = fixedAmount_inputElement.value;
    console.log(`owner : ${ownerName} , fixed amount :${fixedAmount}`);
 
-   let rate = +12.5;
+
    let annual_interest = (+fixedAmount*12.5)/100;
-  let account =  new Account_Details(ownerName,+fixedAmount,rate,annual_interest)
+   let account =  new Account_Details(ownerName,+fixedAmount,rate,annual_interest);
+   accounts.push(account);
+   console.log(accounts);
+   updateTable();
 });
+
+updateInterestBtn.addEventListener('click',()=>{
+   let new_rate = newInterestRate_inputElement.value;
+   rate = +new_rate;
+   // for (let i = 0; i < accounts.length; i++) {
+   //    accounts[i].interest_rate=rate;
+   // }
+   accounts.map(r =>{
+      r.interest_rate=rate;
+      r.annual_interest=(r.fixed_amount*r.interest_rate)/100;
+      new_accounts.push(r);
+   });
+   accounts=new_accounts;
+   updateTable();
+});
+
+function updateTable() {
+   table_body.innerHTML="";
+
+   accounts.map(r => {
+      let record_element = document.createElement('tr');
+      let record_data = `<td>${r.owner_name}</td>
+                         <td>${r.fixed_amount}</td>
+                         <td>${r.interest_rate}</td>
+                         <td>${r.annual_interest}</td>`;
+
+      record_element.innerHTML = record_data;
+      table_body.appendChild(record_element);
+   });
+}
 
 class Account_Details {
    owner_name:string;
