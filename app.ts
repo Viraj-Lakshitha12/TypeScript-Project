260a51b546c0f10c174b2ec803a8916f51658222
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-02 09:09:38 +0530

    update bank.html file

--- app.ts
+++ app.ts
@@ -9,5 +9,22 @@ addFix_btn.addEventListener('click',() =>{
    let fixedAmount = fixedAmount_inputElement.value;
    console.log(`owner : ${ownerName} , fixed amount :${fixedAmount}`);
 
-
+   let rate = +12.5;
+   let annual_interest = (+fixedAmount*12.5)/100;
+  let account =  new Account_Details(ownerName,+fixedAmount,rate,annual_interest)
 });
+
+class Account_Details {
+   owner_name:string;
+   fixed_amount:number;
+   interest_rate:number;
+   annual_interest:number;
+
+   constructor(owner_name:string,fixed_amount:number,interest_rate:number,annual_interest:number) {
+      this.owner_name=owner_name;
+      this.fixed_amount=fixed_amount;
+      this.interest_rate=interest_rate;
+      this.annual_interest=annual_interest;
+   }
+
+}
